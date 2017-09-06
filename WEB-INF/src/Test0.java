c314446f4479a6f7483a44e06a2c7fbd3d674128
Moritz <moritz@liegmanns.de>
2017-09-06 20:40:35 +0200

    Testklasse erweitert

--- WEB-INF/src/Test0.java
+++ WEB-INF/src/Test0.java
@@ -1,4 +1,8 @@
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Map;
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -6,40 +10,45 @@ import javax.servlet.http.*;
 public class Test0 extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        Map<String, String[]> params = request.getParameterMap();
-        for (Map.Entry<String, String[]> entry : params.entrySet()) {
-            entry.getKey();
-        }
-
-        response.setContentType("text/html");
+        response.setContentType("text/plain");
 
         PrintWriter out = response.getWriter();
-        out.append("<html>")
-                .append("<body>")
-                .append("<b>Hello, world!</b>")
-                .append("<p>Request URI: ")
+
+        out.append("Hello, world!")
+                .append("\n")
+                .append("Request URI: ")
                 .append(request.getRequestURI())
-                .append("</p>")
-                .append("<p>Protocol: ")
+                .append("\n")
+                .append("Protocol: ")
                 .append(request.getProtocol())
-                .append("</p>")
-                .append("<p>PathInfo: ")
+                .append("\n")
+                .append("PathInfo: ")
                 .append(request.getPathInfo())
-                .append("</p>")
-                .append("<p>Remote Address: ")
+                .append("\n")
+                .append("Remote Address: ")
                 .append(request.getRemoteAddr())
-                .append("</p>")
-//                .append("</body>")
-//                .append("</html>");
+                .append("\n")
+                .append("\n")
                 .flush();
 
+        Map<String, String[]> params = request.getParameterMap();
+        for (Map.Entry<String, String[]> entry : params.entrySet()) {
+            out.append(entry.getKey())
+                    .append('=');
+            for (String s : entry.getValue())
+                out.append(s);
+            out.append("\n");
+        }
+        out.flush();
+
         try {
-            Thread.sleep(1500);
-        } catch (InterruptedException e) {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/d02566f2", "Moritz", "tracy310");
+            Statement statement = connection.createStatement();
+            statement.execute("SELECT DISTINCT mid, uid, mtext, cid, UNIX_TIMESTAMP(mdate) as mdate FROM Messages WHERE cid = 1 ORDER BY cid, mdate");
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        out.append("<p>TEST</p>").flush();
         out.close();
     }
 
