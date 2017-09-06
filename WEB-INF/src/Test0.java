9b9c0408a98e4a3d1d212c2ddaf60ba696cf376e
Moritz <moritz@liegmanns.de>
2017-09-06 21:56:09 +0200

    Nachrichten empfangen funktioniert soweit

--- WEB-INF/src/Test0.java
+++ WEB-INF/src/Test0.java
@@ -1,9 +1,8 @@
 import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
@@ -14,43 +13,48 @@ public class Test0 extends HttpServlet {
 
         PrintWriter out = response.getWriter();
 
-        out.append("Hello, world!")
-                .append("\n")
-                .append("Request URI: ")
-                .append(request.getRequestURI())
-                .append("\n")
-                .append("Protocol: ")
-                .append(request.getProtocol())
-                .append("\n")
-                .append("PathInfo: ")
-                .append(request.getPathInfo())
-                .append("\n")
-                .append("Remote Address: ")
-                .append(request.getRemoteAddr())
-                .append("\n")
-                .append("\n")
-                .flush();
-
         Map<String, String[]> params = request.getParameterMap();
-        for (Map.Entry<String, String[]> entry : params.entrySet()) {
-            out.append(entry.getKey())
-                    .append('=');
-            for (String s : entry.getValue())
-                out.append(s);
-            out.append("\n");
-        }
+        int uid = Integer.parseInt(params.get("uid")[0]);
+        System.out.println(uid);
         out.flush();
 
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/d02566f2", "Moritz", "tracy310");
-            Statement statement = connection.createStatement();
-            statement.execute("SELECT DISTINCT mid, uid, mtext, cid, UNIX_TIMESTAMP(mdate) as mdate FROM Messages WHERE cid = 1 ORDER BY cid, mdate");
+            boolean running = true;
+            Properties properties = new Properties();
+            properties.setProperty("user", "Moritz");
+            properties.setProperty("password", "tracy310");
+            properties.setProperty("useSSL", "false");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/d02566f2", properties);
+            String date = "0";
+            while (running) {
+                Statement statement = connection.createStatement();
+                if (statement.execute("SELECT mid, mtext, m.cid, m.uid, UNIX_TIMESTAMP(mdate) as mdate FROM Messages m INNER JOIN Assoziation a ON a.cid = m.cid WHERE a.uid = " + uid + " AND UNIX_TIMESTAMP(mdate) > " + date + " ORDER BY mdate DESC")) {
+                    ResultSet resultSet = statement.getResultSet();
+                    if (resultSet.first()) {
+                        date = resultSet.getString(5);
+                        for (; !resultSet.isAfterLast(); resultSet.next()) {
+                            out.append(resultSet.getString(1))
+                                    .append("_;_")
+                                    .append(resultSet.getString(2))
+                                    .append("_;_")
+                                    .append(resultSet.getString(5))
+                                    .append("_;_")
+                                    .append(resultSet.getString(3))
+                                    .append("_;_")
+                                    .append(resultSet.getString(4))
+                                    .append("_nextMessage_");
+                        }
+                        out.flush();
+                    }
+                    resultSet.close();
+                }
+                statement.close();
+            }
+            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
         out.close();
     }
-
-
 }
