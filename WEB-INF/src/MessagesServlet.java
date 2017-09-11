288932d509b5630ecc0ded09ced86a18ac118897
Moritz <moritz@liegmanns.de>
2017-09-11 11:34:15 +0200

    web.xml verändert und leoapp.war neu erzeugt

--- WEB-INF/src/MessagesServlet.java
+++ WEB-INF/src/MessagesServlet.java
@@ -1,5 +1,4 @@
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,7 +8,6 @@ import java.sql.*;
 import java.util.Map;
 import java.util.Properties;
 
-@WebServlet("")
 public class MessagesServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
